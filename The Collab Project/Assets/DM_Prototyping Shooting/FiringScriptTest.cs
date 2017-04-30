078dc10af51af2fc84d0b623916a657ae703b278
Johnny Dalvi <joaozinhoogrande@hotmail.com>
2017-04-30 08:28:10 -0300

    Darren's starting prototype

---
+++ The Collab Project/Assets/DM_Prototyping Shooting/FiringScriptTest.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringScriptTest : MonoBehaviour {
+	
+	[Header ("Play Tweaks")]
+
+	public float chargeTime 				= 0.2f;
+	public float timeBetweenShots 	= 2f;
+	public float shotDuration 				= 1f;
+
+	[Header ("Audio Clips Array")]
+	public AudioClip[] audioClips;
+
+	[Header ("Populate the gun barrel position!")]
+	public Transform gunBarrel;
+
+	[Header ("Debug information")]
+	[SerializeField]private int EnemiesInRangeCount; // just for debug
+	[SerializeField]private Transform currentTarget;
+
+	private Dictionary<int, Transform> EnemiesInRange = new Dictionary<int, Transform>();
+	private List<int> enemyKeysList = new List<int>();
+	private bool isFiring = false;
+	private AudioSource myAudioSource_SFX;
+	private LineRenderer lineRenderer;
+
+
+
+	void Start () {
+		myAudioSource_SFX = GetComponent<AudioSource>();
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+
+	void Update () {
+		// for debug
+		EnemiesInRangeCount = EnemiesInRange.Count; // just for debug
+
+		if (EnemiesInRange.Count >0 && !isFiring)
+		{
+			enemyKeysList.Clear();
+			foreach( int key in EnemiesInRange.Keys)
+			{
+				enemyKeysList.Add(key);
+				Debug.Log(key);
+			}
+	
+			int selectedKey = enemyKeysList[Random.Range(0, enemyKeysList.Count)];
+			currentTarget =  EnemiesInRange[selectedKey];
+			Debug.Log(currentTarget);
+			StartCoroutine(FireAtEnemy(currentTarget));
+		}
+	}
+
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(!col.CompareTag("Damagable"))
+		{ return; }
+
+		int id = (col.gameObject.GetHashCode());
+		if(!EnemiesInRange.ContainsKey(id))
+		{
+			EnemiesInRange.Add(id, col.gameObject.transform);
+		}
+	}
+
+
+	void OnTriggerExit(Collider col)
+	{
+		if(!col.CompareTag("Damagable"))
+		{ return; }
+		int id = (col.gameObject.GetHashCode());
+		if(!EnemiesInRange.ContainsKey(id))
+			EnemiesInRange.Remove(id);
+	}
+
+
+	IEnumerator FireAtEnemy(Transform target)
+	{
+		isFiring = true;
+		myAudioSource_SFX.clip = audioClips[0];
+		myAudioSource_SFX.Play();
+		while(myAudioSource_SFX.isPlaying)
+		{
+			yield return null;
+		}
+		// start the firing sound
+		myAudioSource_SFX.clip = audioClips[1];
+		myAudioSource_SFX.Play();
+
+		// create the line now that firing sound is playing
+		lineRenderer.enabled = true;
+		lineRenderer.SetPosition(0, gunBarrel.position);
+
+		// need to keep track of target as it moves
+		while(myAudioSource_SFX.isPlaying)
+		{
+			lineRenderer.SetPosition(1, currentTarget.position);
+			yield return null;
+		}
+		lineRenderer.enabled = false;
+		yield return new WaitForSeconds(timeBetweenShots);
+		isFiring = false;
+	}
+}
---
+++ The Collab Project/Assets/SimpleMoveForward.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveForward : MonoBehaviour {
+
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;		
+	}
+
+}
