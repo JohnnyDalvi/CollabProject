39cb0366a388ae22d77ad3e0c8ad9ec277809cd0
Johnny Dalvi <joaozinhoogrande@hotmail.com>
2017-05-01 16:47:27 -0300

    First Tower Prototype

--- The Collab Project/Assets/EnergyBall.cs
+++ The Collab Project/Assets/EnergyBall.cs
@@ -7,15 +7,29 @@ public class EnergyBall : MonoBehaviour {
     // Darren, I'm just doing this script in order to test the tower animation, you can freely edit it as you want
 
     public GameObject energyBall;
+    public Transform[] lightningPositions;
     public float verticalMovimentLimit;
     public float verticalMovimentSpeed;
     public float rotationSpeed;
+    public GameObject lightningParticle;
     float initialPosition;
     bool goingUp;
     void Start()
     {
         initialPosition = energyBall.transform.position.y;
+        InvokeRepeating("LightningBall", 0, 0.2f);
     }
+
+    void LightningBall()
+    {
+        foreach(Transform lightning in lightningPositions)
+        {
+            float roll = Random.Range(0, 1f);
+            if (roll >= 0.9f)
+                Instantiate(lightningParticle, lightning);
+        }
+    }
+
     void Update ()
     {
         energyBall.transform.Rotate(Vector3.forward * rotationSpeed *Time.deltaTime);
---
+++ The Collab Project/Assets/LightningScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningScript : MonoBehaviour {
+
+    ParticleSystem lightning;
+    void Start ()
+    {
+        lightning = transform.GetChild(0).GetComponent<ParticleSystem>();
+        Destroy(gameObject, lightning.duration + lightning.startLifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
